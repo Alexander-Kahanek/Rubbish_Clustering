f5e63d6473f7259803b2fcad84d8e0dcdc7f99db
Alexander-Kahanek <59942009+Alexander-Kahanek@users.noreply.github.com>
2020-05-19 18:32:23 -0500

    fix

--- clean-script.R
+++ clean-script.R
@@ -5,44 +5,42 @@ options(stringsAsFactors = FALSE)
 library(dplyr)
 library(lubridate)
 
-add_items <- function(data){
-  # function to add rows for each item
-  # loop through index in df
-  
-  for (k in 1:3){ # needed due to re-indexing
-    for (i in 1:nrow(data)){ 
-      # num items > 1
-      if (data[i,'itemsTagged'] > 1){ 
-        
-        num_items = data[i,'itemsTagged']
-        # add a row for each item
-        for (n in 1:num_items){ 
-          data <- data %>% 
-            rbind(c(itemsTagged= as.numeric(1), street= data[i,'street'], rubbishType= data[i,'rubbishType'],
-                    day= data[i,'day'], serverTimeStamp= data[i,'serverTimeStamp'],
-                    city= data[i,'city'], state= data[i,'state'], lat= data[i,'lat'],
-                    long= data[i,'long']))
-          
-        }
-        # delete old row
-        data <- data[-c(i),] %>% 
-          mutate( # change back to character
-            itemsTagged = as.numeric(itemsTagged) 
-          )
-      }
-    }
-  }
-  
-  return (data)
-}
-
 raw <- read.csv("raw/Startup Grind Data Set - Sheet1.csv")
 
 ## number of items are combined
 # using add_items function to add rows for each item
 
 raw <- raw %>% 
-  add_items()
+  (function(data){
+    # function to add rows for each item
+    # loop through index in df
+    
+    for (k in 1:3){ # needed due to re-indexing
+      for (i in 1:nrow(data)){ 
+        # num items > 1
+        if (data[i,'itemsTagged'] > 1){ 
+          
+          num_items = data[i,'itemsTagged']
+          # add a row for each item
+          for (n in 1:num_items){ 
+            data <- data %>% 
+              rbind(c(itemsTagged= as.numeric(1), street= data[i,'street'], rubbishType= data[i,'rubbishType'],
+                      day= data[i,'day'], serverTimeStamp= data[i,'serverTimeStamp'],
+                      city= data[i,'city'], state= data[i,'state'], lat= data[i,'lat'],
+                      long= data[i,'long']))
+            
+          }
+          # delete old row
+          data <- data[-c(i),] %>% 
+            mutate( # change back to character
+              itemsTagged = as.numeric(itemsTagged) 
+            )
+        }
+      }
+    }
+    
+    return (data)
+  })
 
 ## days are categorized wrong
 # creating new date and time columns, also recategorizing days
